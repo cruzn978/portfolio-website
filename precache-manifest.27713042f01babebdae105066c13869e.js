b7869b433a7b7a54435bb885c12554397dff2abb
Nichole Cruzalegui <n.cruzalegui@hotmail.com>
2020-09-07 15:58:44 -0400

    Updates

---
+++ precache-manifest.27713042f01babebdae105066c13869e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a97b5479709434ae2b1ce0913367a43f",
+    "url": "/portfolio-website/index.html"
+  },
+  {
+    "revision": "ec1a8802ffe09c2dc599",
+    "url": "/portfolio-website/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7a5eb7b4e421cd6f6a30",
+    "url": "/portfolio-website/static/js/2.7a937237.chunk.js"
+  },
+  {
+    "revision": "2069192f75c6f3fb6247231eea52f248",
+    "url": "/portfolio-website/static/js/2.7a937237.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec1a8802ffe09c2dc599",
+    "url": "/portfolio-website/static/js/main.5087c1bd.chunk.js"
+  },
+  {
+    "revision": "a77bb18274229563e654",
+    "url": "/portfolio-website/static/js/runtime-main.5816c056.js"
+  },
+  {
+    "revision": "876aeb4a8845bec889389f489e2e8cf8",
+    "url": "/portfolio-website/static/media/Alaska1.876aeb4a.jpg"
+  },
+  {
+    "revision": "4b1ec2a0ded509436faa71c511ac64ba",
+    "url": "/portfolio-website/static/media/PeachyPlant1.4b1ec2a0.png"
+  },
+  {
+    "revision": "05b2c3dcb08193ba65966955df4e024b",
+    "url": "/portfolio-website/static/media/mediamodifier_image.05b2c3dc.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.193d0f668380cb9ff1b6bece53d1a5aa.js"
+  "/portfolio-website/precache-manifest.27713042f01babebdae105066c13869e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio-website/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
